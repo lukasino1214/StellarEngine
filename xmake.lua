4f7a4758cea7f30e968f239126de787928a573dd
lukasino1214 <38435841+lukasino1214@users.noreply.github.com>
2021-11-24 19:10:50 +0100

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -11,7 +11,7 @@ elseif is_mode("release") then
 end
 
 set_optimize("fastest")
-packages = {"glfw", "glm", "spdlog", "yaml-cpp", "fmt", "tinyobjloader", "tinygltf", "vulkan-headers", "vulkan-hpp", "vulkan-tools", "vulkan-validationlayers", "entt", "stb"}
+packages = {"glfw", "glm", "spdlog", "yaml-cpp", "fmt", "tinyobjloader", "tinygltf", "entt", "stb"}
 
 add_requires(packages)
 
